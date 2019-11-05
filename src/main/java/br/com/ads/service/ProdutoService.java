d83ee5244d1d0917c215700bb48f08e6e67edbf9
Daniel Alves Bezerra <47247720+danialvesb@users.noreply.github.com>
2019-11-04 21:04:16 -0300

    Update ProdutoService.java

--- src/main/java/br/com/ads/service/ProdutoService.java
+++ src/main/java/br/com/ads/service/ProdutoService.java
@@ -1,59 +1,118 @@
-package br.com.ads.service;
+package com.example.cadastroproduto.service;
 
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+//import ddm.com.br.ddm_consulta_produto.MyApp;
+//import ddm.com.br.ddm_consulta_produto.R;
+import com.example.cadastroproduto.model.Produto;
+import com.example.cadastroproduto.utils.ConfigSharedPreferences;
+import com.example.cadastroproduto.utils.DateUtil;
+import com.example.cadastroproduto.utils.HttpHelper;
+import com.example.cadastroproduto.utils.NetworkType;
 
-import br.com.ads.domain.*;
-import br.com.ads.repository.ProdutoRepository;
-import br.com.ads.service.exception.ProdutoExistenteException;
-import br.com.ads.service.exception.ProdutoNaoEncontradoException;
 
-@Service
 public class ProdutoService {
 
-	@Autowired
-	private ProdutoRepository repository;
+    public static List<Produto> getProdutos(boolean isForcarAtualizacao) throws IOException {
+        List<Produto> produtos = null;
+        boolean bGet = false;
+        String json = "";
+        String mensagem2 = "";
+        String networkType = NetworkType.getNetworkClass(MyApp.getContext());
 
-	public List<Produto> listar() {
-		return repository.findAll();
-	}
-	
-	public Produto buscar(Long id) {
-		Produto produto = repository.findById(id).orElse(null);
-		
-		if (produto == null) {
-			throw new ProdutoNaoEncontradoException("Produto não encontrado.");
-		}
-		
-		return produto;
-	}
-	
-	public Produto salvar(Produto produto) {
-		verificarExistencia(produto);
-		produto.setId(null);
-		return repository.save(produto);
-	}
-	
-	public Produto atualizar(Long codigo, Produto produto) {
-		Produto produtoSalvo = buscar(codigo);
-		repository.deleteById(produtoSalvo.getId());
-        
-		return repository.save(produto);
-	}
-	
-	public void delete(long id) {
-		
-		buscar(id);
+        String sServidorIP = ConfigSharedPreferences.getString(MyApp.getContext(), "cfgServidorIP");
+        // Log.w("DDM - Log Sandro", "sServidorIP = " + sServidorIP);
 
-		repository.deleteById(id);
+        if (networkType.equals("-") || !networkType.equals("WIFI")) {
+            String mensagem = (networkType.equals("-") ? "Sem conexão com a Internet." : "Conexão " + networkType);
 
-	}
-	
-	private void verificarExistencia(Produto produto) {
-		if (produto.getId() != null && repository.findById(produto.getId()).orElse(null) != null) {
-			throw new ProdutoExistenteException("Produto já cadastrado.");
-		}
-	}
+            if (sServidorIP != null && !sServidorIP.isEmpty())
+                mensagem2 = MyApp.getContext().getResources().getString(R.string.produtoscarregadosdopropriocelular);
+
+            Toast.makeText(MyApp.getContext(), mensagem + "\n" + mensagem2, Toast.LENGTH_LONG).show();
+            isForcarAtualizacao = false;
+        }
+
+        if (isForcarAtualizacao && sServidorIP != null && !sServidorIP.isEmpty()) {
+            HttpHelper helper = new HttpHelper();
+            json = helper.doGet("http://" + sServidorIP + "/ddm-produtos.json");
+            bGet = true;
+        } else {
+            json = getJsonConfiguracao();
+        }
+
+        if (json == null || json.isEmpty()) {
+            if (sServidorIP == null || sServidorIP.isEmpty()) {
+                throw new IOException("IP do servidor não configurado!\nNenhum produto cadastrado no celular.");
+            } else {
+                throw new IOException("Nenhum produto cadastrado no celular.");
+            }
+        } else {
+            produtos = parserJSON(json);
+
+            if (produtos == null || produtos.isEmpty()) {
+                produtos = getListaProdutosConfiguracao();
+            }
+            else {
+                if (bGet) {
+                    ConfigSharedPreferences.setString(MyApp.getContext(), "cfgJsonProdutos", json);
+                    ConfigSharedPreferences.setString(MyApp.getContext(), "cfgDtUltAtz", DateUtil.DataDMY());
+                }
+            }
+        }
+
+        return produtos;
+    }
+
+    private static String getJsonConfiguracao() {
+        String json = ConfigSharedPreferences.getString(MyApp.getContext(), "cfgJsonProdutos");
+        return json;
+    }
+
+    public static List<Produto> getListaProdutosConfiguracao() throws IOException {
+        List<Produto> produtos = null;
+
+        String json = getJsonConfiguracao();
+
+        if (json == null || json.isEmpty()) {
+            throw new IOException("Nenhum produto cadastrado no celular.");
+        }
+
+        produtos = parserJSON(json);
+
+        return produtos;
+    }
+
+    private static List<Produto> parserJSON(String json) {
+        List<Produto> produtos = new ArrayList<>();
+
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONObject obj = root.getJSONObject("produtos");
+            JSONArray jsonProdutos = obj.getJSONArray("produto");
+
+            for (int i = 0; i < jsonProdutos.length(); i++) {
+                JSONObject jsonProduto = jsonProdutos.getJSONObject(i);
+                Produto p = new Produto();
+
+                p.setEan(jsonProduto.optString("ean"));
+                p.setDescricao(jsonProduto.optString("descricao"));
+                p.setPcovenda(jsonProduto.optDouble("pcovenda"));
+
+                produtos.add(p);
+            }
+        } catch (JSONException e) {
+            produtos.clear();
+        }
+
+        return produtos;
+    }
 }
